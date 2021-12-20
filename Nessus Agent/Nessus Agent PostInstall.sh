40a7f64ef89a9b7f46bab60805cf7eff45d132a4
Ray Lyon <ray@raylyon.net>
2021-12-20 09:14:46 -0500

    check install status in script

--- Nessus Agent/Nessus Agent PostInstall.sh
+++ Nessus Agent/Nessus Agent PostInstall.sh
@@ -4,14 +4,19 @@ KEY=""
 HOST=""
 PORT=""
 
+if [ ! -f "/Library/NessusAgent/run/sbin/nessuscli" ]; then
+    echo "Nessus Agent package did not successfully install."
+    echo "Try re-running the policy."
+    exit 1
+fi
+
 if /Library/NessusAgent/run/sbin/nessuscli agent status | grep "Linked to: None"; then
     /Library/NessusAgent/run/sbin/nessuscli agent link \
     --key="$KEY" \
     --host="$HOST" \
     --port="$PORT"
 else
-    echo "Tenable Agent already installed and linked!"
-    echo "Exiting..."
+    echo "Nessus Agent already installed and linked!"
 fi
 
 exit 0
